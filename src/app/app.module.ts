85fd6e221fd0b1029b4bec4458c01e34524a758d
softwareAssasssin <99945568+softwareAssasssin@users.noreply.github.com>
2023-04-24 18:28:27 +0200

    commited changes to UI changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'; // Importing NgModule from @angular/core
+import { BrowserModule } from '@angular/platform-browser'; // Importing BrowserModule from @angular/platform-browser
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import (HttpClientModule) from '@angular/p';
+import { AppRoutingModule } from './app-routing.module'; // Importing AppRoutingModule from ./app-routing.module
+import { AppComponent } from './app.component'; // Importing AppComponent from ./app.component
+import {HttpClientModule} from '@angular/common/http'; // Importing HttpClientModule from @angular/common/http
 
 @NgModule({
-  declarations: [
+  declarations: [ // Declaring components
     AppComponent
   ],
-  imports: [
+  imports: [ // Importing modules
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // Providing services
+  bootstrap: [AppComponent] // Bootstrapping AppComponent
 })
-export class AppModule { }
+export class AppModule { } // Exporting AppModule
